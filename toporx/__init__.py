c648a0a0bfa2fbae6ddc0dfff7995454580f6bc0
aalvarez122 <ext_aalvarez@igcpharma.com>
2026-01-02 21:00:18 -0500

    Add toporx package init

---
+++ toporx/__init__.py
@@ -0,0 +1,16 @@
+"""
+TopoRx: Topological Biomarker Discovery
+========================================
+
+Predicting cancer drug response using Topological Data Analysis.
+
+Author: Angelica Alvarez
+GitHub: https://github.com/aalvarez122/TopoRx
+"""
+
+__version__ = "0.1.0"
+__author__ = "Angelica Alvarez"
+
+from toporx.pipeline import TopoRxPipeline
+
+__all__ = ["TopoRxPipeline"]
